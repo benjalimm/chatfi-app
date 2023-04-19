d9e5f0c0c19338f7ad3e691de2ae2ce191ebe315
Benjamin Lim <keymote@gmail.com>
2023-04-19 00:15:16 -0700

    Add basic ESLint, some errors still

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import ChatView from '@/components/ChatView'
-import DataView from '@/components/DataView'
-import { Message } from '@/schema/Message';
-import Modal from '@/components/Modal'
-
-
-import io from 'socket.io-client'
-import axios from 'axios'
-
-import ContentView from '@/components/ContentView'
+import Head from 'next/head';
+import ContentView from '@/components/ContentView';
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Chatfi</title>
         <meta name="description" content="Query Coinbase's latest 10-Q" />
       </Head>
-      <ContentView/>
-      
+      <ContentView />
     </>
-  )
+  );
 }
